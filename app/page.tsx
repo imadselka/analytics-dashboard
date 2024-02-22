c975fb542838e1047438215cf09aa9b27458835b
Imad Selka <imadselkawi133@gmail.com>
2024-02-22 22:24:13 +0100

    Refactor page component to include a link to analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
-const page = () => {
-  return <div>page</div>;
+import Link from "next/link";
+
+const Page = () => {
+  return (
+    <div className=" text-white">
+      <Link href="/analytics">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Go to analytics
+        </button>
+      </Link>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
